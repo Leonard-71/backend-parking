be90dc7b830fbf092483cafbb59b67319ccd455d
Filip Leonard <filipleonard71@yahoo.com>
2024-12-07 23:28:49 +0200

    fix: middleware & cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { TenantModule } from './tenants/module/tenant.module';
 import { AuthModule } from './auth/module/auth.module';
-import { LoggerMiddleware } from '../src/middleware/logger.middlerware';
+import { LoggerMiddleware } from './middleware/logger.middlerware';
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,32 @@ async function bootstrap() {
 
   const logger = new Logger('Bootstrap');
   logger.log('Starting application...');
-
+/*
   app.enableCors({
     origin: [
-      'http://localhost:3000',
-      'https://frontend-parking-b9lu-pr7v9sbgg-leonard-71s-projects.vercel.app',
+      'http://localhost:3000', 
       "http://localhost:5173",
-      "https://fl-parcare.netlify.app",
-      "https://67541a25c6db14dc679a4f4e--fl-parcare.netlify.app/",
-    "http://localhost:5175"],
+      "http://localhost:5175",
+
+    "https://fl-parcare.netlify.app", 
+    "https://67541a25c6db14dc679a4f4e--fl-parcare.netlify.app"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+*/
+
+
+app.enableCors({
+  origin: [
+    'https://6754b6e3d8f04f866fe76ea2--fl-parcare.netlify.app', // URL-ul Netlify
+    'https://fl-parcare.netlify.app', // Domeniul final (dacă există)
+    'http://localhost:5173', // Pentru dezvoltare locală
+    'http://localhost:3000', // Pentru cereri locale
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: false, 
+});
+
 
   await app.listen(3000);
   logger.log('Application is running on http://localhost:3000');
