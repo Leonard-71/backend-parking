00ee72ae865933ea3d5d2fc01f5fe1a1cf41f0d7
Filip Leonard <filipleonard71@yahoo.com>
2024-12-07 12:21:33 +0200

    cors settings

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,19 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- 
+
   const logger = new Logger('Bootstrap');
   logger.log('Starting application...');
- 
+
   app.enableCors({
-    origin:[ 'http://localhost:3000', 'https://frontend-parking-b9lu-pr7v9sbgg-leonard-71s-projects.vercel.app', "http://localhost:5173"],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  
-    credentials: true, 
+    origin: [
+      'http://localhost:3000',
+      'https://frontend-parking-b9lu-pr7v9sbgg-leonard-71s-projects.vercel.app',
+      "http://localhost:5173",
+      "https://fl-parcare.netlify.app",
+      "https://67541a25c6db14dc679a4f4e--fl-parcare.netlify.app/"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
   await app.listen(3000);
